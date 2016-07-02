e0a98a70928a4699bcb8ddc05f35c4259500a07d
Bjubes <brian2386@gmail.com>
2016-07-02 13:17:47 -0400

    add background. not zooming correctly

---
+++ Assets/FollowUV.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowUV : MonoBehaviour {
+
+	public float parralax = 0.25f;
+
+	void Update () {
+
+		MeshRenderer mr = GetComponent<MeshRenderer>();
+
+		Material mat = mr.material;
+
+		Vector2 offset = mat.mainTextureOffset;
+
+		offset.x = transform.position.x / transform.localScale.x / parralax;
+		offset.y = transform.position.y / transform.localScale.y / parralax;
+
+		mat.mainTextureOffset = offset;
+
+
+		//scale down based on ortho size
+		Vector3 quadScale = transform.localScale;
+		quadScale.y = Camera.main.orthographicSize * 2;
+		quadScale.x = quadScale.y * Camera.main.aspect;
+		transform.localScale = quadScale;
+	}
+
+}
